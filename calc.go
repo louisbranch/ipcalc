05146d6b10e2b102cf69f1cd6555b2e0ba4d7555
Luiz Branco <me@luizbranco.com>
2016-04-13 17:07:32 -0400

    Stuff

--- calc.go
+++ calc.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"math"
 	"net"
 )
 
 func calculateSubnets(network Network) ([]Subnet, error) {
-	_, ipnet, err := net.ParseCIDR(network.IP)
+	ip, ipnet, err := net.ParseCIDR(network.IP)
 	if err != nil {
 		return nil, err
 	}
@@ -15,11 +16,11 @@ func calculateSubnets(network Network) ([]Subnet, error) {
 	var subnets []Subnet
 
 	mask := ipnet.Mask
+	ones, bits := mask.Size()
+	max := bits - ones
 
 	for _, subnet := range network.Subnets {
 		size := subnet.Size + 2
-		ones, bits := mask.Size()
-		max := bits - ones
 
 		req := int(math.Ceil(math.Log2(float64(size))))
 		size = pow2(req)
@@ -29,9 +30,14 @@ func calculateSubnets(network Network) ([]Subnet, error) {
 			return nil, fmt.Errorf(msg, subnet.Size, (pow2(max))-2)
 		}
 
-		subnet.Size = size
+		n := binary.BigEndian.Uint32(ip.To4()) + uint32(size)
+		ip = net.IPv4(byte(n>>24), byte(n>>16), byte(n>>8), byte(n))
 
-		fmt.Printf("Required %d, ones: %d, bits: %d\n", req, ones, bits)
+		subnet.Size = size
+		subnet.Mask = net.IP(mask).String()
+		subnet.IP = ip.String()
+
+		//fmt.Printf("IP: %s %d\n", ip, num)
 
 		subnets = append(subnets, subnet)
 	}
@@ -41,4 +47,13 @@ func calculateSubnets(network Network) ([]Subnet, error) {
 
 func pow2(n int) int {
 	return int(math.Pow(2, float64(n)))
+}
+
+func intToIP(n int) net.IP {
+	ip := make(net.IP, 4)
+	ip[0] = byte(n)
+	ip[1] = byte(n >> 8)
+	ip[2] = byte(n >> 16)
+	ip[3] = byte(n >> 24)
+	return ip
 }
